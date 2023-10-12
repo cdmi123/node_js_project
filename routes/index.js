499f6ffe2113f4e0a4a14be030a681ea247e65a7
parth-borda <parthborda63@gmail.com>
2023-10-12 09:07:03 +0530

    msg

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const multer = require('multer');
 
-/* image upload */
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  var upload = multer({ storage: storage })
-
-/* end image upload */
 
 
 var user = require('../controller/registrationcontroller');
 
-router.post('/insert', upload.single('image'),user.register);
+router.post('/insert',user.register);
 
 router.get('/',user.select);
 
